5567a94bbb7f0e91f7289ffec360cbe59010609f
blair <blair.values@gmail.com>
2019-12-14 12:08:24 +0800

    new init

---
+++ Array/1_removeDuplicates.py
@@ -0,0 +1,33 @@
+# -*- coding:utf-8 -*-
+
+# Topic: Remove Duplicates from Sorted Array
+
+import sys
+
+class Solution:
+
+    def removeDuplicates(self, nums: list):
+        """
+        :type nums: list[int]
+        :rtype: int
+        """
+
+        i = 0
+        while i < len(nums) - 1:
+            if nums[i] == nums[i + 1]:
+                nums.remove(nums[i])
+            else:
+                i = i + 1
+
+        return len(nums)
+
+if __name__ == '__main__':
+
+    print(sys.version)
+    # print(sys.version_info)
+
+    s = Solution()
+
+    new_len = s.removeDuplicates([1, 1, 2])
+
+    print(new_len)
---
+++ README.md
@@ -0,0 +1,5 @@
+# LeetCode
+
+📩 To the one with a hammer, everything looks like a nail.
+
+## 1. top-interview-questions-easy
