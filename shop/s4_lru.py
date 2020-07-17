b83c62aaea4607d1428718cec9acf20ab61235ed
blair <blair.values@gmail.com>
2020-07-17 10:48:23 +0800

    add a article or modify article

--- shop/s4_lru.py
+++ shop/s4_lru.py
@@ -25,6 +25,29 @@
 #         if len(self) > self.capacity:
 #             self.popitem(last=False)
 
+# ---------------------------------------------------------------------------
+
+# 进阶:
+#
+# 你是否可以在 O(1) 时间复杂度内完成这两种操作？
+#
+#  
+#
+# 示例:
+#
+# LRUCache cache = new LRUCache( 2 /* 缓存容量 */ );
+#
+# cache.put(1, 1);
+# cache.put(2, 2);
+# cache.get(1);       // 返回  1
+# cache.put(3, 3);    // 该操作会使得关键字 2 作废
+# cache.get(2);       // 返回 -1 (未找到)
+# cache.put(4, 4);    // 该操作会使得关键字 1 作废
+# cache.get(1);       // 返回 -1 (未找到)
+# cache.get(3);       // 返回  3
+# cache.get(4);       // 返回  4
+
+
 class DLinkedNode:
     def __init__(self, key=0, value=0):
         self.key = key
---
+++ shop/s8_copy-list-with-random-pointer.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+    @file: s8_copy-list-with-random-pointer.py
+    @date: 2020-07-16 11:56 AM
+"""
+
+# https://leetcode-cn.com/problems/copy-list-with-random-pointer/solution/fu-zhi-dai-sui-ji-zhi-zhen-de-lian-biao-by-leetcod/
