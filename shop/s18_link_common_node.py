ae780d24f7155ffd5cb1f8201a22381acab84c29
blair <blair.values@gmail.com>
2020-07-28 23:39:42 +0800

    add s18

--- shop/s18_link_common_node.py
+++ shop/s18_link_common_node.py
@@ -3,7 +3,25 @@
     @file: s18_link_common_node.py
     @date: 2020-07-27 11:22 AM
 """
+
+
 # https://leetcode-cn.com/problems/liang-ge-lian-biao-de-di-yi-ge-gong-gong-jie-dian-lcof/
 
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+        node1, node2 = headA, headB
+
+        while node1 != node2:
+            node1 = node1.next if node1 else headB
+            node2 = node2.next if node2 else headA
+
+        return node1
 
 
+if __name__ == '__main__':
+    print("hello")
