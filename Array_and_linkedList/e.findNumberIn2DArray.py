c2e6d770a2ae5ef45b0a6e3e738139283ccdd5ed
blair <blair.values@gmail.com>
2020-09-11 09:04:30 +0800

    add a article or modify resources

---
+++ Array_and_linkedList/e.findNumberIn2DArray.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+    @file: e.findNumberIn2DArray
+    @date: 2020-09-11 8:44 PM
+    @desc: 剑指 Offer 04. 二维数组中的查找
+    @url : https://leetcode-cn.com/problems/er-wei-shu-zu-zhong-de-cha-zhao-lcof/
+"""
+from typing import List
+
+
+class Solution:
+    def findNumberIn2DArray(self, matrix: List[List[int]], target: int) -> bool:
+        pass
---
+++ Array_and_linkedList/e.minStack.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+    @file: e.MinStack
+    @date: 2020-09-11 8:44 PM
+    @desc: 剑指 Offer 30. 包含min函数的栈
+    @url : https://leetcode-cn.com/problems/bao-han-minhan-shu-de-zhan-lcof/
+"""
+
+class MinStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+
+
+    def push(self, x: int) -> None:
+        pass
+
+
+    def pop(self) -> None:
+        pass
+
+
+    def top(self) -> int:
+        pass
+
+
+    def min(self) -> int:
+        pass
+
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.min()
