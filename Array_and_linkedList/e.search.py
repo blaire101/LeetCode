c7f5671c9ed8fd1fffcff7b8f7297a1f84d00df6
blair <blair.values@gmail.com>
2020-09-11 09:02:04 +0800

    add a article or modify resources

---
+++ Array_and_linkedList/e.search.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+    @file: e.search
+    @date: 2020-09-11 8:44 PM
+    @desc: 剑指 Offer 53 - I. 在排序数组中查找数字 I
+    @url : https://leetcode-cn.com/problems/zai-pai-xu-shu-zu-zhong-cha-zhao-shu-zi-lcof/
+"""
+from typing import List
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        pass
