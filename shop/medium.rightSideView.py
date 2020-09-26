111d316135b30a28a597d6cd04f59bd8dbd6cd96
blair <blair.values@gmail.com>
2020-09-26 14:25:05 +0800

    add a article or modify article

--- shop/medium.rightSideView.py
+++ shop/medium.rightSideView.py
@@ -8,6 +8,36 @@
 
 # Definition for a binary tree node.
 from typing import List
+from collections import deque
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+# 输入: [1,2,3,null,5,null,4]
+# 输出: [1, 3, 4]
+# 解释:
+#
+#    1            <---
+#  /   \
+# 2     3         <---
+#  \     \
+#   5     4       <---
+
+# -*- coding: utf-8 -*-
+"""
+    @file: medium.rightSideView.py
+    @date: 2020-09-25 7:47 AM
+    @desc: 199. 二叉树的右视图
+    @url : https://leetcode-cn.com/problems/binary-tree-right-side-view/
+"""
+
+# Definition for a binary tree node.
+from typing import List
+from collections import deque
 
 
 class TreeNode:
@@ -28,4 +58,22 @@ class TreeNode:
 
 class Solution:
     def rightSideView(self, root: TreeNode) -> List[int]:
-        pass
+        if not root:
+            return []
+
+        res = {}
+
+        deq = deque()
+        level = 1
+
+        deq.append([root, level])
+        while deq:
+            cur_node, cur_level = deq.popleft()
+            res[cur_level] = cur_node.val
+            if cur_node.left:
+                deq.append([cur_node.left, cur_level+1])
+            if cur_node.right:
+                deq.append([cur_node.right, cur_level + 1])
+
+        return [res[k] for k in res]
+
--- shop/medium.sortList-for-linkedlist.py
+++ shop/medium.sortList-for-linkedlist.py
@@ -3,6 +3,7 @@
     @file: medium.sortList-for-linkedlist.py
     @date: 2020-09-25 7:46 AM
     @desc: 148. 排序链表
+    @answ: https://leetcode-cn.com/problems/sort-list/solution/sort-list-gui-bing-pai-xu-lian-biao-by-jyd/
     @url : https://leetcode-cn.com/problems/sort-list/
 """
 
@@ -21,4 +22,21 @@ class ListNode:
 
 class Solution:
     def sortList(self, head: ListNode) -> ListNode:
-        pass
+        if not head or not head.next: return head # termination.
+        # cut the LinkedList at the mid index.
+        slow, fast = head, head.next
+        while fast and fast.next:
+            fast, slow = fast.next.next, slow.next
+        mid, slow.next = slow.next, None # save and cut.
+        # recursive for cutting.
+        left, right = self.sortList(head), self.sortList(mid)
+        # merge `left` and `right` linked list and return it.
+        h = res = ListNode(0)
+        while left and right:
+            if left.val < right.val: h.next, left = left, left.next
+            else: h.next, right = right, right.next
+            h = h.next
+        h.next = left if left else right
+        return res.next
+
+
