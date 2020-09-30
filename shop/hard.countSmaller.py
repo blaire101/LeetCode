ac06827403850ccec87c06d1d73d50bf4bdc6ad6
blair <blair.values@gmail.com>
2020-09-30 09:17:13 +0800

    add a article or modify resources

---
+++ shop/hard.countSmaller.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+    @file: hard.countSmaller.py
+    @date: 2020-09-30 08:38 AM
+    @desc: 315. 计算右侧小于当前元素的个数
+    @url : https://leetcode-cn.com/problems/count-of-smaller-numbers-after-self/
+"""
+from typing import List
+
+
+class Solution:
+    def countSmaller(self, nums: List[int]) -> List[int]:
+        pass
